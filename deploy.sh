1c1f0e4c9e89a6618fc98527baa5968440043569
Cam Tousi <ctousi@gmail.com>
2023-08-29 21:27:08 -0400

    Updated deploy.sh and added langchain schema directory

--- deploy.sh
+++ deploy.sh
@@ -6,4 +6,6 @@ pip install --upgrade pip
 # Install requirements from the specific directory
 pip install -r docs/api_reference/requirements.txt
 
+export PYTHONPATH="${PYTHONPATH}:/opt/render/project/src/libs/"
+
 # Add any other setup or run commands you need
