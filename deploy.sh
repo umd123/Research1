d33ff4a6efbc15a00061e36b5910ee0e5cd3e5d9
Cam Tousi <70666094+umd123@users.noreply.github.com>
2023-09-03 19:58:27 -0400

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,34 @@
 #!/bin/bash
 
+# Add a log message indicating the start of the script
+echo "Starting deployment script..."
+
+# Log the current pip version
+echo "Current pip version:"
+pip --version
+
+# Upgrade pip and log the action
 echo "Upgrading pip..."
 pip install --upgrade pip
 echo "Pip upgraded."
 
-echo "Installing requirements..."
+# Log the new pip version
+echo "New pip version:"
+pip --version
+
+# Install requirements and log the action
+echo "Installing requirements from docs/api_reference/requirements.txt..."
 pip install -r docs/api_reference/requirements.txt
 echo "Requirements installed."
 
+# Upgrade langchain and log the action
+echo "Upgrading langchain..."
+pip install langchain --upgrade
+echo "Langchain upgraded."
+
+# Set the PYTHONPATH and log the action
 export PYTHONPATH="${PYTHONPATH}:/opt/render/project/src/libs/"
 echo "PYTHONPATH set to: $PYTHONPATH"
 
-# Add any other setup or run commands you need
+# Add a log message indicating the end of the script
+echo "Deployment script completed."
