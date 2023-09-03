9dac5fbc7a6ac5a48de73b033a2301b2293a6d0c
Cam Tousi <70666094+umd123@users.noreply.github.com>
2023-09-03 19:51:56 -0400

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-# Upgrade pip
+echo "Upgrading pip..."
 pip install --upgrade pip
+echo "Pip upgraded."
 
-# Install requirements from the specific directory
+echo "Installing requirements..."
 pip install -r docs/api_reference/requirements.txt
+echo "Requirements installed."
 
 export PYTHONPATH="${PYTHONPATH}:/opt/render/project/src/libs/"
+echo "PYTHONPATH set to: $PYTHONPATH"
 
 # Add any other setup or run commands you need
